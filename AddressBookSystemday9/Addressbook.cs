0940045cd390a10e70af534d81f5c64502a559bb
shubham07h <shubham1609sharma@gmail.com>
2022-12-10 01:55:09 +0530

    commit

--- AddressBookSystemday9/Addressbook.cs
+++ AddressBookSystemday9/Addressbook.cs
@@ -50,7 +50,69 @@ namespace AddressBookSystemday9
                 Console.WriteLine("Phonenumber - " + records.PhoneNumber);
                 Console.WriteLine("Email - " + records.Email);
             }
+
+
+        }
+        public void EditContact()
+        {
+            Console.WriteLine("To edit contact details enter contact firstname");
+            string name = Console.ReadLine();
+            foreach (var record in data)
+            {
+                if (record.Firstname == name)
+                {
+                    Console.WriteLine(name + " is exists");
+                    Console.WriteLine("To edit details Enter\n1Lastname\n2.Address\n3.City\n4.State\n5.Zip\n6.Phonenumber\n7.Email");
+                    int choice = Convert.ToInt32(Console.ReadLine());
+                    switch (choice)
+                    {
+                        case 1:
+                            Console.WriteLine("Enter new Lastname");
+                            string newLastname = Console.ReadLine();
+                            record.Lastname = newLastname;
+                            break;
+                        case 2:
+                            Console.WriteLine("Enter new Address");
+                            string newAddress = Console.ReadLine();
+                            record.Address = newAddress;
+                            break;
+                        case 3:
+                            Console.WriteLine("Enter new City");
+                            string newCity = Console.ReadLine();
+                            record.City = newCity;
+                            break;
+                        case 4:
+                            Console.WriteLine("Enter new State");
+                            string newState = Console.ReadLine();
+                            record.State = newState;
+                            break;
+                        case 5:
+                            Console.WriteLine("Enter new Zip");
+                            int newZip = Convert.ToInt32(Console.ReadLine());
+
+                            record.Zip = newZip;
+                            break;
+                        case 6:
+                            Console.WriteLine("Enter new Phonenumber");
+                            int newPhonenumber = Convert.ToInt16(Console.ReadLine());
+                            record.PhoneNumber = newPhonenumber;
+                            break;
+                        case 7:
+                            Console.WriteLine("Enter new Email");
+                            string newEmail = Console.ReadLine();
+                            record.Email = newEmail;
+                            break;
+                    }
+                    return;
+                }
+                else
+                {
+                    Console.WriteLine("Name does not exists");
+                }
+            }
         }
 
     }
 }
+
+    
